a686a44bf00b94ee55cef5fa3254ad3dae493186
andifirwansyah <andifirwansyah250@gmail.com>
2023-08-22 15:53:06 +0700

    init

--- components/TheHero.vue
+++ components/TheHero.vue
@@ -6,7 +6,7 @@
           <div class="text-paraph mt-5 font-arian-demi text-lg xl:w-4/12 w-full">{{ $t('home.about_short') }}</div>
           <button class="bg-primary rounded-lg px-5 py-3 text-white text-xl mt-4">{{ $t('home.read_more') }}</button>
       </div>
-      <img src="~/assets/images/hero-hand.png" class="xl:flex object-contain absolute right-0 z-0 bottom-0 2xl:w-[58.813rem] md:w-[35rem]">
+      <img src="~/assets/images/hero-hand.png" class="xl:flex object-contain absolute right-0 z-0 bottom-0 2xl:w-[58.813rem] md:w-[35rem] w-[20rem]">
     </div>
   </template>
   
