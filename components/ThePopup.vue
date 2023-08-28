de892a55b8cf75e2648e3f99349af57a46d7c28a
andifirwansyah <andifirwansyah250@gmail.com>
2023-08-28 15:52:43 +0700

    fix popup

--- components/ThePopup.vue
+++ components/ThePopup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="absolute w-full h-screen xl:px-0 px-10 bg-black top-0 z-50 flex items-center justify-center bg-opacity-10 custom-drop-blur">
+    <div class="absolute w-full h-screen xl:px-0 px-5 bg-black top-0 z-50 flex items-center justify-center bg-opacity-10 custom-drop-blur">
         <div class="bg-white w-[30rem] rounded-3xl custom-shadow pb-14 relative">
             <div class="bg-[url('~/assets/images/group-391.png')] rounded-t-3xl w-full xl:h-64 h-40 bg-cover bg-no-repeat flex justify-center relative">
                 <div class="absolute right-2 top-2 cursor-pointer w-8 h-8 rounded-full flex items-center justify-center border-2 border-white" @click="$emit('close-modal')">
@@ -12,7 +12,9 @@
                 </svg>
             </div>
             <div class="text-center text-5xl mt-3 text-primary font-arian-bold">{{ $t('home.warning') }}</div>
-            <div class="text-center font-arian-bold justify-custom mx-10 mt-3 text-slate-700">{{ $t('home.warning_content') }}</div>
+            <div class="font-arian-bold mt-3 px-5 text-slate-700">
+                <p class="text-justify text-base">{{ $t('home.warning_content') }}</p>
+            </div>
             <div class="flex mt-5 items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 text-primary h-6">
                     <path fill-rule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clip-rule="evenodd" />
